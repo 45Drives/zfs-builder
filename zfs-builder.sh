a1b0390ea2f5ec5afef326290585d298cd28ac3f
hpham64 <hpham@45drives.com>
2026-01-16 13:21:53 -0400

    Initial commit

---
+++ zfs-builder.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+# ZFS building packages
+
+install_zfs() {
+
+	local err
+
+	#Retrieve OS variables
+	source /etc/os-release
+
+	#Clone ZFS repo
+	echo "Cloning ZFS repo"
+	git clone https://github.com/openzfs/zfs.git
+
+	#Switch to release branch to build (2.3.5)
+	cd ./zfs && git checkout zfs-2.3-release
+
+	#Install dependencies
+	echo "Installing dependencies"
+	dnf install --skip-broken epel-release gcc make autoconf automake libtool rpm-build libtirpc-devel libblkid-devel libuuid-devel libudev-devel openssl-devel zlib-devel libaio-devel libattr-devel elfutils-libelf-devel kernel-devel-$(uname -r) python3 python3-devel python3-setuptools python3-cffi libffi-devel git ncompress libcurl-devel
+	err=$?
+        if [[ $err != 0 ]]; then
+                echo "Installing dependencies failed"
+                exit $err
+        fi
+	#Check for EL8 or EL9
+	if [[ "$ID" == "rocky" ]]; then
+		version=${VERSION_ID%%.*}
+
+		if [[ "$version" == "8" ]]; then
+			echo "Installing packages for EL8 Rocky"
+			dnf install --skip-broken --enablerepo=epel --enablerepo=powertools python3-packaging dkms
+			err=$?
+        		if [[ $err != 0 ]]; then
+                		echo "Installing ZFS repo failed"
+                		exit $err
+        		fi
+
+		elif [[ "$version" == "9" ]]; then
+			echo "Installing packages for EL9 Rocky"
+			dnf config-manager --enable crb
+			dnf install --skip-broken --enablerepo=epel python3-packaging dkms
+			err=$?
+        		if [[ $err != 0 ]]; then
+		                echo "Installing ZFS repo failed"
+                		exit $err
+		        fi
+
+		else
+			echo "Unsupported version"
+			return 1
+		fi
+		
+	#Different OS
+	else
+		echo "Not Rocky, exiting..."
+		return 1
+	fi
+
+	#Run autogen script
+	sh autogen.sh
+
+	#Configure
+	./configure
+
+	#Build ZFS in-tree
+	make -s -j"$(nproc)"
+
+	#Install userland tools
+	make install
+
+	#Update module dependencies and load ZFS
+	depmod -a
+
+	#Starting ZFS manually
+	modprobe zfs
+	
+	#Configure ZFS to load on boot
+	echo zfs > /etc/modules-load.d/zfs.conf
+	
+	#Check ZFS version
+	echo "ZFS Version:"
+	zfs --version
+}
+
+# Call function
+install_zfs
